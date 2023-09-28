bd9ceeaccc5730dfc9b50e290bd865cbf6123b28
rightone0130 <rightone0130@gmail.com>
2023-09-28 10:54:42 -0700

    added components

---
+++ ats-app/src/components/navBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import '../navBar'
+
+
+
+
+
+const NavBar = () => {
+
+
+
+
+
+return(
+
+
+    <>
+    
+    <div class="arrow-div">
+  <p>Your content here</p>
+</div>
+
+
+    </>
+)
+
+
+
+}
+
+
+
+export default NavBar
---
+++ ats-app/src/components/sideBar/SideBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const SideBar = () => {
+
+return(
+
+<>
+
+
+</>
+
+)
+
+
+}
+
+
+
+export default SideBar
